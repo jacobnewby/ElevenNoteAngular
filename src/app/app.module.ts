f0a03d0a2a22d2dfc6f5930b8f1e3c8516eaf31b
jacobnewby <jacobnewby01@gmail.com>
2019-11-07 15:47:49 -0500

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations"
+import { RouterModule, Router } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+
+import { 
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule, 
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { NoopAnimationPlayer } from '@angular/animations';
+import { AuthService } from './services/auth.service';
+import { NoteService } from './services/note.service';
+import { LoginComponent } from './components/login/login.component';
+import { NoteIndexComponent } from './components/note/note-index/note-index.component';
+
+const routes = [
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    LoginComponent,
+    NoteIndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    NoteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/note/note-index/note-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-index',
+  templateUrl: './note-index.component.html',
+  styleUrls: ['./note-index.component.css']
+})
+export class NoteIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
